9095d906313fdfc850eb0c2f1b1ce722b6588400
Matteo Muscas <thatman.j@gmail.com>
2018-12-17 08:41:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,10 @@
 class BankAccount
-  attr_accessor :account_name
+  attr_accessor :account_name, :balance, :status
 
   def initialize(account_name)
     @account_name = account_name
-    balance = 1000
-    status = open
+    @balance = 1000
+    @status = open
   end
 
 
